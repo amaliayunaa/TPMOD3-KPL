98b30fbb0533bfdc0bad64687b1fe79908d51c0d
amaliayunaa <malyayn20@gmail.com>
2022-03-11 15:45:07 +0700

    menambah git

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace tpModul3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            KodePos KodePos = new KodePos();
+            KodePos.getKodePos("Kelurahan", "Kode Pos");
+            KodePos.getKodePos("Batununggal", "40266");
+            KodePos.getKodePos("Kujangsari", "40287");
+            KodePos.getKodePos("Mengger", "40267");
+            KodePos.getKodePos("Wates", "40256");
+            KodePos.getKodePos("Cijaura", "40287");
+            KodePos.getKodePos("Jatisari", "40286");
+            KodePos.getKodePos("Margasari", "40286");
+            KodePos.getKodePos("Sekejati", "40286");
+            KodePos.getKodePos("Kebonwaru", "40272");
+            KodePos.getKodePos("Maleer", "40274");
+            KodePos.getKodePos("Samoja", "40273");
+
+            DoorMachine.Mulai();
+        }
+    }
+
+
+}
